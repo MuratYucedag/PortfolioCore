8a0226774e311d57546b1893045bfe90f4211550
MuratYucedag <yucedagmurat23@gmail.com>
2025-04-02 21:58:32 +0300

    Add project files.

---
+++ PortfolioCore/Entities/Education.cs
@@ -0,0 +1,17 @@
+﻿namespace PortfolioCore.Entities
+{
+    public class Education
+    {
+        public int EducationId { get; set; }
+        public string Title { get; set; }
+        public string DateInfo { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+    }
+}
+/*
+ 1-Lisans
+ 2-Yüksek Lisans
+ 3-Doktora
+ ....
+ */
