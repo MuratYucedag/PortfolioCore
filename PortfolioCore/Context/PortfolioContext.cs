0eae49809cffdc6a42638f833f0f81270c97f54c
MuratYucedag <yucedagmurat23@gmail.com>
2025-04-09 21:52:33 +0300

    ...

---
+++ PortfolioCore/Context/PortfolioContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioCore.Entities;
+
+namespace PortfolioCore.Context
+{
+    public class PortfolioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-R7AR1ND;initial Catalog=PortfolioNightDb;integrated security=true;");
+        }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
+//trust server certificate=true;
